e25ef1d5e2bd26a31141cd6d28e4fce4d33f73d9
mengps <843261040@qq.com>
2019-10-05 20:52:29 +0800

    更新:完整的SkinManager

--- src/skinmanager.cpp
+++ src/skinmanager.cpp
@@ -1,15 +1,58 @@
 #include "skinmanager.h"
 
+#include <QFile>
+
 SkinManager* SkinManager::instance()
 {
-    static SkinManager setting;
-    return &setting;
+    static SkinManager skin;
+    return &skin;
 }
 
 SkinManager::~SkinManager()
 {
+
+}
+
+/**
+ * @brief 两种读取方式
+ *   READ(globalColor)
+ *   else READ(menuColor)
+ * 或者(性能稍差)
+ *   globalColorRead();
+ *   menuColorRead();
+ */
+void SkinManager::loadSkin(const QString &skinName)
+{
+    QFile file(skinName);
+    if (!file.open(QIODevice::ReadOnly)) {
+        qDebug() << file.errorString();
+        return;
+    }
+
+    m_reader.setDevice(&file);
+    if (m_reader.readNextStartElement()) {
+        qDebug().noquote().nospace() << "<===Load Skin: " << m_reader.name() << "===>";
+        while(!m_reader.atEnd()) {
+            m_reader.readNext();
+            if (m_reader.isStartElement() && m_reader.name() == QLatin1String("color")) {
+                globalColorRead(); detailColorRead(); borderColorRead();
+                menuColorRead(); menuBackColorRead(); menuHoverColorRead();
+                headerColorRead(); headerBorderColorRead(); headerHoverColorRead();
+                progressColorRead(); progressBackColorRead(); progressHandleColorRead();
+                buttonColorRead(); buttonBackColorRead();
+                tooltipColorRead(); tooltipBorderColorRead();
+                textColorRead(); musicTextColorRead(); musicTextSelectedColorRead();
+            } else if (m_reader.isStartElement() && m_reader.name() == QLatin1String("image")) {
+                backgroundImageRead(); progressHandleImageRead();
+                buttonParticleImageRead(); progressParticleImageRead();
+            }
+        }
+    }
+
+    file.close();
 }
 
 SkinManager::SkinManager()
 {
+    loadSkin(":/skin/default_black.skin");
 }
--- src/skinmanager.h
+++ src/skinmanager.h
@@ -1,18 +1,65 @@
 #ifndef SKINMANAGER_H
 #define SKINMANAGER_H
 
+#include <QDebug>
 #include <QObject>
+#include <QUrl>
+#include <QXmlStreamReader>
+
+#define READ(attr) \
+    if (m_reader.attributes().value(QLatin1String("title")) == QLatin1String(#attr)) { \
+        m_##attr = m_reader.readElementText(); \
+        emit attr##Changed(); \
+        qDebug().noquote().nospace() << #attr << ": " << m_##attr; \
+    }
+
+#define COLOR(color) \
+    Q_PROPERTY(QString color READ color NOTIFY color##Changed) \
+    Q_SIGNALS: void color##Changed(); \
+    public: QString color() const { return m_##color; } \
+    private: void color##Read() { READ(color); } \
+        QString m_##color;
+
+#define IMAGE(image) \
+    Q_PROPERTY(QUrl image READ image NOTIFY image##Changed) \
+    Q_SIGNALS: void image##Changed(); \
+    public: QUrl image() const { return m_##image; } \
+    private: void image##Read() { READ(image); } \
+        QUrl m_##image;
 
 class SkinManager : public QObject
 {
     Q_OBJECT
 
+    //Panel
+    COLOR(globalColor) COLOR(detailColor) COLOR(borderColor)
+    //Menu
+    COLOR(menuColor) COLOR(menuHoverColor) COLOR(menuBackColor)
+    //Header
+    COLOR(headerColor) COLOR(headerHoverColor) COLOR(headerBorderColor)
+    //Progress
+    COLOR(progressColor) COLOR(progressBackColor) COLOR(progressHandleColor)
+    //Button
+    COLOR(buttonColor) COLOR(buttonBackColor)
+    //Tooltip
+    COLOR(tooltipColor) COLOR(tooltipBorderColor)
+    //Text
+    COLOR(textColor) COLOR(musicTextColor) COLOR(musicTextSelectedColor)
+    //Image
+    IMAGE(backgroundImage) IMAGE(progressHandleImage)
+    //ParticleImage
+    IMAGE(buttonParticleImage) IMAGE(progressParticleImage)
+
 public:
     static SkinManager* instance();
     ~SkinManager();
 
+    Q_INVOKABLE void loadSkin(const QString &skinName);
+
 private:
     SkinManager();
+
+    QXmlStreamReader m_reader;
 };
 
 #endif //SKINMANAGER_H
